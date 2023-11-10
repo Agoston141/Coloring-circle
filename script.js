8546d67c5d01ca003bf71ca00471cb15220ab2b4
Agoston141 <molnar.agoston2006@gmail.com>
2023-11-10 14:19:37 +0100

    kész-slider

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const button =document.querySelector("button");
 
-button.onclick = ()=>{
+const inputs = document.querySelectorAll("input");
+inputs.forEach(inp =>{
+    const red = document.querySelector("input:first-child");
+    const green = document.querySelector("input:nth-child(2)");
+    const blue = document.querySelector("input:last-of-type");
+    const circle= document.querySelector(".circle");
+    inp.addEventListener("input",()=>{
+        circle.style.backgroundColor=`rgb(${red.value}, ${green.value}, ${blue.value})`
+    })
+})
+
+
+/*button.onclick = ()=>{
     const red = document.querySelector("input:first-child");
     const green = document.querySelector("input:nth-child(2)");
     const blue = document.querySelector("input:last-of-type");
@@ -8,4 +20,4 @@ button.onclick = ()=>{
 
     circle.style.backgroundColor=`rgb(${red.value}, ${green.value}, ${blue.value})`
 }
-
+*/
